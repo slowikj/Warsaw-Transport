91abaa345686922393fff0fa11bc0d3d013f76f9
slowikj <jadwis.slowik@gmail.com>
2017-05-28 09:06:13 +0200

    Create the opportunity to use data frames

--- symulator.py
+++ symulator.py
@@ -10,7 +10,7 @@ from tkinter import *
 import time
 import json
 import sqlite3
-
+import pandas as pd
 
 def _create_circle(self, x, y, r, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
@@ -20,8 +20,10 @@ class converterMap:
     y1 = 52.160088
     x2 = 21.227348
     y2 = 52.330103
-    length = 1280
-    width = 810
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
     
     def isOnMap(self, lon, lat):
         if(self.x1 < lon < self.x2 and self.y1 < lat < self.y2):
@@ -30,31 +32,82 @@ class converterMap:
             return False
     
     def getPosition(self, lon, lat):
-        x = (lon - self.x1) / (self.x2 - self.x1) * self.length
-        y = self.width - (lat - self.y1) / (self.y2 - self.y1) * self.width
+        x = (lon - self.x1) / (self.x2 - self.x1) * self.width
+        y = self.height - (lat - self.y1) / (self.y2 - self.y1) * self.height
         return (x,y)
 
-root = Tk()
-root.geometry('2480x1240')
-Canvas.create_circle = _create_circle
-canvas = Canvas(root,width=2480,height=1240)
-canvas.pack()
-temp=PIL.Image.open("mapa_warszawy.jpg")
-temp = temp.save("photo.ppm","ppm")
-photo = PhotoImage(file = "photo.ppm")
-cm = converterMap()
-conn = sqlite3.connect("autobusyPiatek")
-conn2 = sqlite3.connect("tramwajePiatek")
-for row in conn2.execute("SELECT * FROM data"):
-    canvas.create_image(640, 405, image=photo)
-    data = json.loads(row[1])
-    currList = data['result']
-    for currItem in currList:
-        if(cm.isOnMap(float(currItem['Lon']), float(currItem['Lat']))):
-            p = cm.getPosition(float(currItem['Lon']), float(currItem['Lat']))
-            canvas.create_circle(p[0], p[1], 2, fill="blue", width=1)
-    root.update()
-    time.sleep(0.5)
-    canvas.delete("all")
+def get_dataframe_from_db(db_name, table_name, json_column_names):
+	with sqlite3.connect(db_name) as con:
+		cur = con.cursor()
+		cur.execute("SELECT * FROM " + table_name)
+		all_rows = cur.fetchall()
 
-root.mainloop()
+		df = pd.DataFrame(columns=json_column_names)
+
+		for (cnt, results_string) in all_rows:
+			results_json = json.loads(results_string)
+			json_array = results_json["result"]
+
+			for json_item in json_array:
+				df.loc[len(df)] = json_item
+
+	return df
+
+def create_image_from_data_frame(data_frame,
+                                 background_image_path, image_width, image_height):
+    (root, canvas, photo) = get_drawing_data(background_image_path, image_width, image_height)
+    
+    cm = converterMap(image_width, image_height)   
+    
+    for row in data_frame.iterrows():
+        row_series = row[1]
+        lineNumber = (row_series["Lines"].strip())
+        longitude = float(row_series["Lon"])
+        latitude = float(row_series["Lat"])
+        
+        if cm.isOnMap(longitude, latitude):
+            p = cm.getPosition(longitude, latitude)
+            canvas.create_circle(p[0], p[1], 2, fill="blue", width=1)
+        
+
+    root.update()
+    root.mainloop()
+            
+
+def get_drawing_data(background_image_path, image_width, image_height):
+    root = Tk()
+    canvas_width = get_canvas_width(image_width)
+    canvas_height = get_canvas_height(image_height)
+    root.geometry(str(canvas_width) + "x" + str(canvas_height))
+    
+    Canvas.create_circle = _create_circle
+    canvas = Canvas(root,width=canvas_width,height=canvas_height)
+    canvas.pack()
+    
+    temp=PIL.Image.open(background_image_path)
+    temp = temp.save("photo.ppm","ppm")
+    photo = PhotoImage(file = "photo.ppm")
+    
+    canvas.create_image(image_width / 2, image_height / 2, image=photo)
+    
+    return (root, canvas, photo)
+    
+
+def get_canvas_width(image_width):
+    return image_width + 10
+
+def get_canvas_height(image_height):
+    return image_height + 10
+
+df = get_dataframe_from_db("autobusyPiatek", "data", ["Lat", "Lon", "Time", "Lines", "Brigade"])
+
+df_523 = df.loc[df.Lines == "523",:]
+
+create_image_from_data_frame(df_523,
+                             "mapa_warszawy.jpg",
+                             1280, 810)
+
+
+
+
+
